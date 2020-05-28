e8c4d7e63bf4e0b75e5070e8bc8a456aadb771a2
David Strube <dstrube@gmail.com>
2020-05-27 22:57:22 -0400

    torchStuff

---
+++ torchStuff.py
@@ -0,0 +1,12 @@
+#file:///C:/Users/dstrube3/Documents/GitHub/test/masters/Deep-Learning-with-PyTorch.pdf
+#https://pytorch.org/tutorials/beginner/deep_learning_60min_blitz.html
+#torchStuff.py
+
+#import numpy
+#print("I can import numpy")
+
+#import torch
+#print("I can import torch")
+
+#pip3 install torch --user
+
