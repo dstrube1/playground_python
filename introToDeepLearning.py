6b9b0e13a8dea100b6f1f021c25141e2b91ddc31
David Strube <dstrube@gmail.com>
2020-05-28 09:57:19 -0400

    introToDeepLearning

---
+++ introToDeepLearning.py
@@ -0,0 +1,34 @@
+#introToDeepLearning.py
+# Scikit-learn, TensorFlow and tf.keras
+
+import sklearn
+
+import tensorflow as tf
+
+from tensorflow import keras 
+
+# Helper libraries
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+print("verion of sklearn: " + sklearn.__version__)
+
+print("version of tensorflow: " + tf.__version__)
+
+#https://www.tensorflow.org/tutorials/keras/classification
+#->Run in Google Colab
+
+#https://playground.tensorflow.org
+
+#https://www.cs.ryerson.ca/~aharley/vis/conv/
+
+#https://www.tensorflow.org/tutorials/images/cnn
+
+#https://projector.tensorflow.org/
+#search for "apple" - doesn't find fruit :\
+
+#https://www.tensorflow.org/tutorials/keras/text_classification
+#https://www.tensorflow.org/tutorials/text/text_classification_rnn
+
