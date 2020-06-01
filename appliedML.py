fc35693d7ad78ac2a861508978d0309176958f4c
David Strube <dstrube@gmail.com>
2020-06-01 11:23:21 -0400

    appliedML

---
+++ appliedML.py
@@ -0,0 +1,9 @@
+#appliedML
+
+#Begin: clone this:
+#https://github.com/chrisblanton/gatech_machine_learning
+#after cloning that, go to Terminal, cd to that dir, then run this:
+#jupyter notebook
+
+#data source:
+#https://www.kaggle.com/
