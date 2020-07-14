a46dd1c77fe8957aba932a5fd514d5e526dad132
David Strube <dstrube@gmail.com>
2020-07-14 09:29:03 -0400

    Minor edits

--- djangoTest/testDjango.py
+++ djangoTest/testDjango.py
@@ -1,5 +1,8 @@
 #testDjango.py
 import django
 
+#https://docs.djangoproject.com/en/3.0/intro/tutorial01/
+
 print("Can import django")
 print("version: " + django.get_version())
+#2020-07-10: 3.0.6
