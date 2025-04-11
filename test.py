819cb92d70332af6f447340cf6c0462e5ab9f19f
David Strube <dstrube3@gatech.edu>
2025-04-11 08:42:00 -0400

    Update test.py

--- test.py
+++ test.py
@@ -73,4 +73,75 @@ def rangeTest():
 	print("range(1,4):")
 	for x in range(1,4):
 		print(x) #1,2,3
-rangeTest()
+#rangeTest()
+
+def createStaircase(nums):
+	"""INCORRECT:
+	while len(nums) != 0:
+		step = 1
+		subsets = []
+		if len(nums) > step:
+			subsets.append(nums[0:step])
+			nums = nums[step:]
+			step += 1
+		else:
+			return False
+	return True"""
+	step = 1
+	subsets = []
+	while len(nums) != 0:
+		if len(nums) >= step:
+			subsets.append(nums[0:step])
+			nums = nums[step:]
+			step += 1
+		else:
+			return False
+	return subsets
+
+#print (createStaircase([1,2,3,4,5,6]))
+
+from bs4 import BeautifulSoup
+# pip install bs4
+import requests
+
+def printFromURL(url):
+	try:
+		# got this much from emailScraper:
+		response = requests.get(url) 
+		#print(response.text)
+		soup = BeautifulSoup(response.text, features="html.parser") 
+		#tables = soup.find_all("table")
+		#print(tables)
+		
+		# this is from here:
+		# https://stackoverflow.com/questions/45843025/parsing-html-tables-with-beautifulsoup-in-python
+		table_data = [i.text for i in soup.find_all('td')]
+		
+		# throw away the header row
+		table_data = table_data[3:]
+		#print(table_data)
+		
+		# Convert list into coordinate-character pairs
+		points = [(int(table_data[i]), int(table_data[i+2]), table_data[i+1]) for i in range(0, len(table_data), 3)]
+		print (points)
+		
+		# Determine grid size
+		max_x = max(x for x, y, char in points)
+		max_y = max(y for x, y, char in points)
+		
+		# Create grid with default space character
+		grid = [[' ' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+		
+		# Fill grid with characters
+		for x, y, char in points:
+			grid[y][x] = char
+		
+		# Print the grid
+		for row in grid:
+			print(''.join(row))
+		
+	except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+		print("Caught exception")
+
+url = "https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub"
+printFromURL(url)
