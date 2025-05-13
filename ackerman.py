8c1a60785d20e9f7a60f96acbde401ab63b2f766
David Strube <dstrube3@gatech.edu>
2025-05-13 08:14:19 -0400

    Update ackerman.py

--- ackerman.py
+++ ackerman.py
@@ -7,8 +7,9 @@ def ack(m,n):
 	elif n == 0:
 		return ack(m-1,1)
 	else:
+		print("return ack(m-1, ack(m,n-1) => ack(" + str(m) + "-1, ack(" + str(m) + "," + str(n) + "-1)")
 		return ack(m-1, ack(m,n-1))
 
 for m in range(5):
 	for n in range(5):
-		print("ack("+str(m)+","+str(n)+"): " + str(ack(m,n)))
+		print("ack(" + str(m) + "," + str(n) + "): " + str(ack(m,n)))
