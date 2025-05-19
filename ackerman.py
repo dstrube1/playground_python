be2e7e906a82797421ecd90796601c6817837b40
David Strube <dstrube3@gatech.edu>
2025-05-19 15:12:27 -0400

    Update ackerman.py

--- ackerman.py
+++ ackerman.py
@@ -1,15 +1,17 @@
 #ackerman.py
 #https://www.youtube.com/watch?v=i7sm9dzFtEI
 
-def ack(m,n):
+def ack(m,n,level):
 	if m == 0:
 		return n+1
 	elif n == 0:
-		return ack(m-1,1)
+		level += 1
+		return ack(m-1,1,level)
 	else:
-		print("return ack(m-1, ack(m,n-1) => ack(" + str(m) + "-1, ack(" + str(m) + "," + str(n) + "-1)")
-		return ack(m-1, ack(m,n-1))
+		print("return ack(m-1, ack(m,n-1,level),...) => ack(" + str(m) + "-1, ack(" + str(m) + "," + str(n) + "-1," + str(level) + ")")
+		level += 1
+		return ack(m-1, ack(m,n-1,level),level)
 
 for m in range(5):
 	for n in range(5):
-		print("ack(" + str(m) + "," + str(n) + "): " + str(ack(m,n)))
+		print("ack(" + str(m) + "," + str(n) + "): " + str(ack(m,n,0)))
