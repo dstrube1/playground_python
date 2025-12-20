f34abd9e9b523cd79ba0f3ad858685aed07a308a
David Strube <dstrube3@gatech.edu>
2025-12-20 14:51:56 -0500

    Update harshad.py

--- harshad.py
+++ harshad.py
@@ -27,10 +27,50 @@ def main():
 			return
 		harshad_bases = []
 		for base in range(2,21):
+# ===========================================================
+# New
+			if base == 10:
+				if is_harshad_in_base_10(number):
+					harshad_bases.append(base)
+				# regardless of the result of is_harshad_in_base_10, 
+				# skip to the next iteration of the loop, 
+				# because we don't want to test base 10 twice
+				continue
+# END New
+# ===========================================================
 			if is_harshad_in_base(number, base):
 				harshad_bases.append(base)
 		print(f"\n{number} is a Harshad number in the following bases: {harshad_bases}" ) 
 	except ValueError:
 		print("Invalid input. Try again.")
+
+# ===========================================================
+# New stuff, i.e., mine
+# at 1:02: "any number which is (evenly) divisible by the sum of its digits is a harshad number"
+def get_digits_in_base_10(n):
+	digits = list(str(n))
+	# digits[::-1] <- this reverses the items in the list, but why?
+	return digitis if digits else [0]
+
+def digits_sum_in_base_10(n):
+	# get list of characters in the number
+	digits_str = get_digits_in_base_10(n)
+	# convert the characters to integers
+	digits = []
+	for digit in digits_str:
+		digits.append(int(digit))
+	return sum(digits)
+
+def is_harshad_in_base_10(n):
+	print("base = 10, using is_harshad_in_base_10...")
+	digit_sum = digits_sum_in_base_10(n)
+	if digit_sum == 0:
+		return False # avoid divide-by-zero
+	# No remainder => evenly divisible
+	return n % digit_sum == 0
+
+
+# ===========================================================
+
 if __name__ == "__main__":
 	main()
